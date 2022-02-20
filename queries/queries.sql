32550093228ebdcdaf68a55fdd077893bac05ba7
Alexandra Curtis <ajcurtis916@gmail.com>
2022-02-20 12:09:44 -0800

    add dept_retirees and update query

--- queries/queries.sql
+++ queries/queries.sql
@@ -67,3 +67,83 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no=dept_emp.emp_no;
+
+SELECT * FROM retirement_info;
+
+--Use aliases and write query into retiement_info table
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no=de.emp_no;
+
+DROP TABLE current_emp;
+--Only a query up until this point
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+--Does not change data in tables until this line of code
+--Puts query into new table
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+--Filters for only current employees
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+SELECT COUNT (*)
+FROM current_emp;
+
+--Employee count by department number
+SELECT COUNT(ce.emp_no),de.dept_no
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no=de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Create new table for query for export
+SELECT COUNT(ce.emp_no),de.dept_no
+INTO dept_retirees
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no=de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM dept_retirees;
